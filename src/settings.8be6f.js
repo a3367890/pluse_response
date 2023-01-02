659ab2002de155b25ec065a9a7d85c9b86f30b42
glenn <glenn0047@gmail.com>
2023-01-02 16:14:43 +0800

    reflash

--- src/settings.8be6f.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scenes/page0.fire",orientation:"",jsList:[],bundleVers:{internal:"279e0",resources:"01701",main:"c055e"}};
